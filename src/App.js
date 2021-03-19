bb5c2f7a296e824db7324fcd5052ceda63815c00
jihyeonoh12 <jihyeonoh12@gmail.com>
2021-03-18 22:59:55 -0700

    edited again

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { HashRouter, Route, Link } from "react-router-dom";
+import {  HashRouter , Route } from "react-router-dom";
 import './App.css';
 import Home from './pages/Home'
 import About from './pages/About'
@@ -11,11 +11,8 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
-      <HashRouter basename="/portfolio-app">
-      <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/about">About</Link></li>
-      </ul>
+     <HashRouter>
+
            <Route path="/" component={Home} exact/>
            <Route path="/about" component={About}/>
            <Route path="/digital-design" component={About}/>
@@ -23,8 +20,8 @@ class App extends React.Component {
            <Route path="/coding" component={About}/>
            <Route path="/post-protein-plus" component={PostProteinPlus}/>
           <Route component={Error}/>
-     
-        </HashRouter>
+          </HashRouter>
+
       </div> 
 
   )
